fd5dc207f79f6b57520912adcc6f1be005ca2718
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2019-08-28 12:04:58 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict survival on the Titanic and get familiar with ML basics',
+    author='Fabrice NIYOMWUNGERI',
+    license='MIT',
+)
